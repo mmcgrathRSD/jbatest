893e7e3114b6e306391eb46c863b335097d80a14
Ubuntu <ubuntu@ip-172-31-32-221.us-west-2.compute.internal>
2020-05-24 21:38:07 +0000

    some fixes

--- apps/JBAShop/Services/Magento.php
+++ apps/JBAShop/Services/Magento.php
@@ -480,6 +480,7 @@ class Magento
         WHERE
             def.attribute_id = 102
             AND def.store_id = 0
+            AND def.entity_id = 11687
             ORDER BY cpe.sku ASC
         ";
 
