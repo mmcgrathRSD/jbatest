e3a3d9dbf4f476c31a4d1af7ba5a2135a14ab1b4
Christopher West <9ninemind@gmail.com>
2020-06-05 21:24:59 +0000

    styles email validation page

--- apps/Theme/Overrides/Users/Site/Views/login/validate.php
+++ apps/Theme/Overrides/Users/Site/Views/login/validate.php
@@ -1,26 +1,37 @@
-<div class="container">
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <legend>
-                Email Verification
-            </legend>
-            
-            <p>We have sent you an email to verify your identity.  Please click the link inside the email to confirm your email address and activate your account.</p>
-            
-            <p>Alternatively, you may copy and paste the Token from the email into this form.</p> 
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-            <form action="./login/validate" method="post" class="form" role="form">
-                <div class="form-group">
-                    <label>Token</label>
-                    <input class="form-control" name="token" placeholder="Token from email" type="text" />
+<div class="customer-account-login">
+    <div class="content-container">
+        <div class="main row clearfix">
+            <div class="col-main grid_18">
+                <div class="account-login">
+                    <div class="block block-login">
+                        <div class="jcarousel-container jcarousel-container-horizontal" style="position: relative; display: block;">
+                            <div class="jcarousel-clip jcarousel-clip-horizontal" style="position: relative;">
+                                <ul class="slides jcarousel-list jcarousel-list-horizontal" style="overflow: hidden; position: relative; top: 0px; margin: 0px; padding: 0px; left: 0px; width: 640px;">
+                                    <li class="jcarousel-item jcarousel-item-horizontal jcarousel-item-1 jcarousel-item-1-horizontal" jcarouselindex="1" style="float: left; list-style: none;">
+                                        <div class="block-title">
+                                            <strong><span>Please verify your email address</span></strong>
+                                        </div>
+                                        <div class="block-content">
+                                            <p>We have sent you an email to verify your identity.  Please click the link inside the email to confirm your email address and activate your account.</p>
+
+                                            <p>Alternatively, you may copy and paste the token from the email into this form.</p> 
+
+                                            <form action="./login/validate" method="post" class="form" role="form">
+                                                <label for="token" class="required">Token</label>
+                                                <div class="input-box">
+                                                    <input type="text" name="token" id="token" class="input-text required-entry" title="Email Token" required>
+                                                </div><br>
+                                                
+                                                <button type="submit" class="button" title="Submit"><span><span>Submit</span></span></button>
+                                            </form>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                            
-                <button class="btn btn-lg btn-primary" type="submit">Submit</button>
-                
-            </form>
+            </div>
         </div>
     </div>
 </div>
