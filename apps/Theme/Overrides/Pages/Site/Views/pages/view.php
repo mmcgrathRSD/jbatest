0719e4ee22bcf4c9566283bab9d7445b7387e443
Christopher West <9ninemind@gmail.com>
2020-06-11 17:19:32 +0000

    adds 404 page and centers pages

--- apps/Theme/Overrides/Pages/Site/Views/pages/view.php
+++ apps/Theme/Overrides/Pages/Site/Views/pages/view.php
@@ -1,2 +1,11 @@
-<?php echo $item->{'copy'}; ?>
+<div class="content-container">
+    <div class="main row">
+        <div class="col-main grid_13 custom_right">
+            <div class="std">
+                <?php echo $item->{'copy'}; ?>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div style="clear:both; padding-bottom: 20px;"></div>
--- apps/Theme/Views/404.php
+++ apps/Theme/Views/404.php
@@ -1,22 +1,49 @@
-<ol class="breadcrumb row ">
-	<li><a href="./shop">Shop</a></li>
-  
-   <li> Page Not Found</a></li>
-</ol>
-
-
-<div class="row paddingBottom">
-	<div class="col-xs-12 text-center">
-			<h3> <i class="icon-exclamation"></i> Oops! We couldn't find the page you're looking for... </h3>
-			<p>Search above or go back to our <a href="/">homepage</a> </p>
-			
-			<tmpl type="modules" name="404-end" />
-		
-
-			<?php   $f3 = \Base::instance();
-			        //fixing google base links
-			     	$path = $f3->hive()['PATH']; ?>
-
+<div class="content-container">
+    <div class="main row">
+        <div class="col-main grid_13 custom_right">
+            <div class="std">
+                <div class="page-title">
+                    <h1>Whoops, our bad. . .</h1>
+                </div>
+                <dl>
+                    <dt>The page you requested was not found, and we have a fine guess why.</dt>
+                    <dd>
+                        <ul class="disc">
+                            <li>If you typed the URL directly, please make sure the spelling is correct.</li>
+                            <li>If you clicked on a link to get here, the link is outdated.  ATTEMPT to refresh (F5) the page once.  If you still come up with the same error, then there is most likely not a server error.</li>
+                        </ul>
+                    </dd>
+                </dl>
+                <dl>
+                    <dt>What can you do?</dt>
+                    <dd>Have no fear, help is near! There are many ways you can get back on track!</dd>
+                    <dd>
+                        <ul class="disc">
+                            <li><a href="#" onclick="history.go(-1); return false;">Go back</a> to the previous page.</li>
+                            <li>Use the search bar at the top of the page to search for your products.</li>
+                            <li>Follow these links to get you back on track!<br><a href="/">Store Home</a> <span class="separator">|</span> <a href="/shop/account/">My Account</a></li>
+                        </ul>
+                    </dd>
+                </dl>
+            </div>
+            <div id="map-popup" class="map-popup" style="display:none;">
+                <a href="#" class="map-popup-close" id="map-popup-close">x</a>
+                <div class="map-popup-arrow"></div>
+                <div class="map-popup-heading">
+                    <h2 id="map-popup-heading"></h2>
+                </div>
+                <div class="map-popup-content" id="map-popup-content">
+                    <div class="map-popup-checkout">
+                        <form action="" method="POST" id="product_addtocart_form_from_popup">
+                            <input type="hidden" name="product" class="product_id" value="" id="map-popup-product-id">
+                            <div class="additional-addtocart-box">
+                            </div>
+                            <button type="button" title="Add to Cart" class="button btn-cart" id="map-popup-button" onclick=""><span><span>Add to Cart</span></span></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-right sidebar grid_5 custom_right"></div>
+    </div>
 </div>
-</div>
-<!-- Page Content Container -->
