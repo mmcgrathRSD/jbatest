28c001ef701be12a2e750301de5ecfd461ee2bc5
Christopher West <9ninemind@gmail.com>
2020-06-11 22:41:34 +0000

    adds ymm info (unstyled) to product pages

---
+++ apps/Theme/Overrides/Shop/Site/Views/product/blocks/new_confirmed_fitment_inner.php
@@ -0,0 +1,37 @@
+<?php
+    $cache = \Cache::instance();
+    if (!$cache->exists('product_ymms_.' . (string) $item->id, $ymmMapping )) {
+        $ymmMapping = $item->getYMMMapping();
+        $cache->set('product_ymms_.' . (string) $item->id, $ymmMapping, 86400);
+    }
+?>
+
+<?php if (!empty($ymmMapping)) : ?>
+    <hr>
+    <h3>This Part Fits</h3>
+
+    <?php foreach($ymmMapping as $make => $makes) :
+            foreach($makes as $model => $models) : ?>
+                <div class="makeModel <?php echo $make . '_' . $model; ?>">
+                    <span class="make_model"><?php echo $make . ' ' . $model; ?></span>
+                    <span class="makeModelsub">
+                        <div class="make_model_border"></div>
+                    </span>
+                        <?php foreach($models as $subModel => $subModels) : ?>
+                            <span class="makeModelsub">
+                            <span class="subModel"><?php echo $subModel; ?>
+                        <?php foreach($subModels as $info) : ?>
+                            <a href="<?php echo $info['link'] ?>" title="We have <?php echo $info['product_count'] ?> parts that fit the <?php echo $info['year'] . " " . $make . " " . $model . " " . $subModel; ?>">
+                                <div>
+                                    <?php echo $info['year'] . " " . $make . " " . $model . " " . $subModel; ?>
+                                </div>
+                            </a>
+                        <?php endforeach; ?>
+                            </span>
+                        </span>
+                        <?php endforeach; ?>
+                </div>
+            <?php endforeach; ?>
+
+        <?php endforeach; ?>
+<?php endif; ?>
