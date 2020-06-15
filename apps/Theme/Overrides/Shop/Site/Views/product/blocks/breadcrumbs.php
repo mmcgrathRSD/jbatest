e6c46b66de4ae34a326fe6335c6e722d1a2fbf31
Christopher West <9ninemind@gmail.com>
2020-06-15 21:07:41 +0000

    adds breadcrumbs based on how you got to product

--- apps/Theme/Overrides/Shop/Site/Views/product/blocks/breadcrumbs.php
+++ apps/Theme/Overrides/Shop/Site/Views/product/blocks/breadcrumbs.php
@@ -1,12 +1,17 @@
 <div class="breadcrumbs">
     <ul>
-    <li style="display: inline-block;" typeof="v:Breadcrumb">
-        <a href="/" title="Home" rel="v:url" property="v:title">
-        Home                </a>
-        <span>/</span>
-    </li>
-    <li style="display: inline-block;">
-        <strong><?php echo $item->title; ?></strong>
-    </li>
+        <li style="display: inline-block;" typeof="v:Breadcrumb">
+            <a href="/" title="Home" rel="v:url" property="v:title">Home</a>
+            <span>/</span>
+        </li>
+        <?php foreach((array) $breadcrumbs as $link => $title): ?>
+            <li style="display: inline-block;" typeof="v:Breadcrumb">
+                <a href="<?php echo $link ?>" title="<?php echo $title ?>" rel="v:url" property="v:title"><?php echo $title ?></a>
+                <span>/</span>
+            </li>
+        <?php endforeach; ?>
+        <li style="display: inline-block;">
+            <strong><?php echo $item->title; ?></strong>
+        </li>
     </ul>
 </div>
