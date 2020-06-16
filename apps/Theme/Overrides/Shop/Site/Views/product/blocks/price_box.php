d58e5d7cbb70c2861fce4650a24e8ad8c6f11409
Austin Roberts <austin.roberts@rallysportdirect.com>
2020-06-16 21:36:54 +0000

    as low as bug

--- apps/Theme/Overrides/Shop/Site/Views/product/blocks/price_box.php
+++ apps/Theme/Overrides/Shop/Site/Views/product/blocks/price_box.php
@@ -7,7 +7,7 @@ $price = $item->price();
     <div class="f-left">
         <div class="price-box">
             <span class="regular-price">
-            <span class="price"><?php echo $this->renderLayout('Shop/Site/Views::product/blocks/lockup/price.php')?></span>                                    </span>
+                <span class="price <?php if($item->{'product_type'} == 'dynamic_group') : ?>dyn_kit_price<?php endif; ?>"><?php echo $this->renderLayout('Shop/Site/Views::product/blocks/lockup/price.php')?></span>                                    </span>
             <?php if(!empty($previous_price) && $previous_price < $price && filter_var($item->get('flag.enabled'), FILTER_VALIDATE_BOOLEAN)) : ?>
                 <p class="old-price">
                     <span class="price-label"></span>
