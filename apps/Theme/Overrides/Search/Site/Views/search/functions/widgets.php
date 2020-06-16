6376d5c2b9b87a71bc81951ddd26176484b96ee9
Austin Roberts <austin.roberts@rallysportdirect.com>
2020-06-16 18:26:10 +0000

    changing h1 on search

--- apps/Theme/Overrides/Search/Site/Views/search/functions/widgets.php
+++ apps/Theme/Overrides/Search/Site/Views/search/functions/widgets.php
@@ -42,7 +42,7 @@
             <?php if(!$this->app->get('master_search')) : ?>
             <h1><?php echo $item->title; ?> Parts</h1>
             <?php else : ?>
-            <h1>Search</h1>
+            <span class="search_title">Search</span>
             <?php endif; ?>
          </div>
          <div class="category-description std">
