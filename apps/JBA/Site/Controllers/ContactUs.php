bfb4be539c5ad7e7f5b3065bc9fd0c3a3704c881
Christopher West <9ninemind@gmail.com>
2020-06-17 19:23:45 +0000

    JBA-452&JBA-253 adds contact us page w/ zendesk integration

--- apps/JBA/Site/Controllers/ContactUs.php
+++ apps/JBA/Site/Controllers/ContactUs.php
@@ -5,72 +5,66 @@ class ContactUs extends \Dsc\Controller
 {
     public function index()
     {
-
-
         $this->app->set('meta.title', 'Contact Us');
         $this->app->set('meta.description', 'Contact JBA');
         
         echo $this->theme->render('JBA\Site\Views::contactus/default.php');
     }
     
-    
-    
-   public function submit() {
-   	
-   		$customer  = $this->input->get('customer', '', 'string');
-   		$email = $this->input->get('email', '', 'string');
-   		$phone = $this->input->get('phone', '', 'string');
-   		$topic = $this->input->get('topic', '', 'string');
-   		$message = nl2br($this->input->get('message', '', 'html'));
-   		
+    public function submit()
+    {
+        $name  = $this->input->get('z_name', '', 'string');
+        $email = filter_var($this->input->get('z_requester', '', 'string'), FILTER_VALIDATE_EMAIL);
+        $order = $this->input->get('z_order', '', 'string');
+        $phone = $this->input->get('z_telephone', '', 'string');
+        $type = $this->input->get('z_drop-down', '', 'string');
+        $subject = $this->input->get('z_subject', '', 'string');
+        $message = $this->input->get('z_description', '', 'string');
 
-   		
-	   	if(!empty($customer) && !empty($email)  && !empty($phone)  && !empty($topic)  && !empty($message)) {
-	   		$toEmail="customerservice@rallysportdirect.com";
-	   		//$toEmail="chris.french@rallysportdirect.com";
-	   		//$toEmail="zac.ranck@rallysportdirect.com";
-            
-	   		$mailer = \Dsc\System::instance()->get('mailer');
-	   		if ($content = $mailer->getEmailContents('rallysport.contact_us_request', array(
-	   				'customername' => $customer,
-	   				'customeremail' => $email,
-	   				'customerphone' => $phone,
-	   				'customertopic' => $topic,
-	   				'customermessage' => $message
-	   		
-	   		))) {
+        if (!empty($name) && !empty($email) && !empty($type)  && !empty($message)) {
+            // Create Zendesk Ticket
+            if ($zendesk = \Dsc\System::instance()->get('zendesk')) {
+                $finalSubject = $type;
+                if (!empty($order)) {
+                    $finalSubject .= " #$order";
+                }
 
-	   			$content['subject'] = $topic;
-	   			$content['replyToEmail'] = $email; 
-	   			$content['replyToName'] = $customer;
-	   			 
-	   			$mailer->sendEvent( $toEmail, $content);
-	   		}
-	   		
-	 
-	   		
+                $finalSubject .= ' - ' . $subject;
 
-	   		if ($this->app->get('AJAX')) {
-	   				return $this->outputJson( $this->getJsonResponse( array(
-	   						'result'=>true,
-	   						'message'=>'Email Sent'
-	   				) ) );
-	   		
-	   		} else {
-	   			\Dsc\System::addMessage('Thank you for contacting JBA. A customer service representative will contact you shortly.','success');
-	   			$this->app->reroute('/contact-us');
-	   		}
+                $finalMessage = $message;
+                if (!empty($phone)) {
+                    $finalMessage .= "\n\n\nTelephone: {$phone}";
+                }
 
-	   	}
-
+                $zendesk->tickets()->create([
+                    'brand_id' => $this->app->get('zendesk_api.brand_id'),
+                    "custom_fields" => [
+                        'id'    => $this->app->get('zendesk_api.custom_fields_id'),
+                        'value' => 'Tracking'
+                    ],
+                    'subject' => $finalSubject,
+                    'comment' => [
+                        'value' => $finalMessage
+                    ],
+                    'requester' => [
+                        'name' => $name,
+                        'email' => $email
+                    ]
+                ]);
+            }
+               
+            \Dsc\System::addMessage('Your inquery was submitted and will be responded to as soon as possible.  Thank you for contacting us.', 'success');
+            $this->app->reroute('/contact-us');
+        } else {
+            \Dsc\System::addMessage('Please fill out all required fields!', 'error');
+            $this->app->reroute('/contact-us');
+        }
    }
    
-   public function returnHandoff() {
-   
-   			\Dsc\System::addMessage('You can create a return from inside your order.','success');
-   			$this->app->reroute('/shop/account');
-   
+   public function returnHandoff()
+   {
+        \Dsc\System::addMessage('You can create a return from inside your order.','success');
+        $this->app->reroute('/shop/account');
    }
-   
 }
 
