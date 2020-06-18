cbf2ced40db273b41326a91e018b0b25f20381f3
Christopher West <9ninemind@gmail.com>
2020-06-18 21:47:51 +0000

    JBA-477 styles password change page

--- apps/Theme/Overrides/Users/Site/Views/change/password.php
+++ apps/Theme/Overrides/Users/Site/Views/change/password.php
@@ -1,34 +1,44 @@
-
-    <ol class="breadcrumb row">
-        <li>
-            <a href="./user">My Account</a>
-        </li>
-        <li class="active">Change Password</li>
-    </ol>
-    
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
-            <legend>
-                Change Password
-            </legend>
+<div class="customer-account-login">
+    <div class="content-container">
+        <div class="main row clearfix">
+            <div class="col-main grid_18">
+                <div class="account-login">
+                    <div class="block block-login">
+                        <div class="jcarousel-container jcarousel-container-horizontal" style="position: relative; display: block;">
+                        <div class="jcarousel-clip jcarousel-clip-horizontal" style="position: relative;">
+                            <ul class="slides jcarousel-list jcarousel-list-horizontal" style="overflow: hidden; position: relative; top: 0px; margin: 0px; padding: 0px; left: 0px; width: 640px;">
+                                <li class="jcarousel-item jcarousel-item-horizontal jcarousel-item-1 jcarousel-item-1-horizontal" jcarouselindex="1" style="float: left; list-style: none;">
+                                    <div class="block-title">
+                                        <strong><span>Change Password</span></strong>
+                                    </div>
+                                    <div class="block-content">
+                                        <form action="./user/change-password" method="post" id="password-form">
+                                            <input name="form_key" type="hidden" value="QMOkWbNbwt6gn3Nm">
+                                            <ul class="form-list">
+                                                <li>
+                                                    <label for="new_password" class="required">New Password</label>
+                                                    <div class="input-box">
+                                                        <input type="password" name="new_password" value="" id="new_password" class="input-text required-entry" title="New Password" required>
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <label for="confirm_new_password" class="required">Confirm New Password</label>
+                                                    <div class="input-box">
+                                                        <input type="password" name="confirm_new_password" value="" id="confirm_new_password" class="input-text required-entry" title="Confirm New Password" required>
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                            <button type="submit" class="button" title="Update Password" name="send" id="send2"><span><span>Update Password</span></span></button>
+                                        </form>
+                                    </div>
+                                </li>
+                                
+                            </ul>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-            <form action="./user/change-password" method="post" class="form" role="form">
-                <div class="form-group">
-                    <label>New Password</label>
-                    <input class="form-control" name="new_password" placeholder="New Password" type="password" required />
-                </div>
-                
-                <div class="form-group">
-                    <label>Confirm New Password</label>
-                    <input class="form-control" name="confirm_new_password" placeholder="Confirm New Password" type="password" required />
-                </div>
-                            
-                <button class="btn btn-lg btn-primary" type="submit">Update password</button>
-                <a class="btn btn-link" href="./user">Cancel</a>
-                
-            </form>
-        </div>
-    </div>
+</div>
