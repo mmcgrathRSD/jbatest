c8180e515f7e8c8953e0d3a918bad47208cb286f
Austin Roberts <austin.roberts@rallysportdirect.com>
2020-06-24 21:42:13 +0000

    removing commented assets, moving amazon to sass

--- apps/Theme/Views/common/head.php
+++ apps/Theme/Views/common/head.php
@@ -28,31 +28,7 @@
 
     //]]>
   </script>
-  <!-- <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/js/olegnax/fancybox/jquery.fancybox-1.3.4.css" />
-  <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/athlete/default/css/styles.css" media="all" />
-  <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/base/default/css/widgets.css" media="all" /> -->
-  <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/base/default/css/mirasvit/searchautocomplete/amazon.css" media="all" />
-  <!-- <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/base/default/css/mirasvit_searchindex.css" media="all" /> -->
-  <!-- <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/base/default/css/mirasvit/seo/mirasvit_seo.css" media="all" /> -->
-  <!-- <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/base/default/css/mirasvit/seositemap/sitemap.css" media="all" /> -->
-  <!-- <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/athlete/default/css/olegnax/ajaxcart.css" media="all" /> -->
 
-  <!-- <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/athlete/default/css/slideshow.css" media="all" />
-  <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/athlete/default/rs-plugin/css/settings.css" media="all" />
-  <link rel="stylesheet" type="text/css" href="/theme/css/jba/megamenu.css?<?php echo $cachebuster; ?>" media="all" />
-  <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/athlete/default/css/local.css" media="all" />
-  <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/athlete/default/css/animation.css" media="all" />
-  <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/athlete/default/css/grid.css" media="all" />
-  <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/athlete/default/css/retina.css" media="all" />
-  <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/athlete/default/css/options_base_default.css" media="all" />
-  <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/athlete/default/css/override.css" media="all" /> -->
-
-  <!-- <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/base/default/js/amasty/amconf/css/amconf.css" media="all" />
-  <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/base/default/js/amasty/amconf/css/tooltipster.css" media="all" /> -->
-  
-  <!-- <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/athlete/default/css/print.css" media="print" />
-  <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/athlete/default/css/magestore/mobilelibrary.css" defer />
-  <link rel="stylesheet" type="text/css" href="https://www.subispeed.com/skin/frontend/athlete/default/css/magestore/giftvoucher.css" defer /> -->
   <link href='//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800' rel='stylesheet' type='text/css'>
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
 
@@ -62,21 +38,14 @@
   <link href="/theme/css/styles.min.css?<?php echo $cachebuster; ?>" rel="stylesheet">
   <?php endif;?>
 
-  <!-- <script
-  src="https://code.jquery.com/jquery-3.4.1.min.js"
-  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-  crossorigin="anonymous"></script> -->
+
   <script src="/shop-assets/js/vendor/cloudinary.core.min.js"></script>
   <?php echo $this->renderView('Assets/Site/Views::common/script_variables.php'); ?>
-  
- <!-- <script src="/shop-assets/js/vendor/jquery-1.11.2.min.js"></script> -->
   <script src="/shop-assets/js/vendor/instantsearch.min.js?<?php echo $cachebuster; ?>" ></script>
   
   <?php if($checkoutmode) : ?>
     <link href="/theme/css/bootstrap.css" rel="stylesheet">
   <?php endif; ?>
-
-  <!-- <script src="/theme/js/jba2.js?<?php echo $cachebuster; ?>"></script> -->
   <script src="/shop-assets/js/vendor/jquery-1.11.2.min.js"></script>
  
   <?php if($DEBUG) :  ?>
