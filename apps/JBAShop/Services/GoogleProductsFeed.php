e70c4a0d5005ee4e0dddb6e0cb0eedf0487140f9
Kiley Holman <kiley.holman@rallysportdirect.com>
2020-07-13 15:41:48 +0000

    Fix images for child items on matrix and group items.

--- apps/JBAShop/Services/GoogleProductsFeed.php
+++ apps/JBAShop/Services/GoogleProductsFeed.php
@@ -49,11 +49,11 @@ class GoogleProductsFeed
 		$this->mate->blue("Generating {$channel->get('slug')} google product feed.");
 		$productsWriter = $this->startXML($name);
 		//generate non matrix and matrix_subitems.
-		$this->groupAndStandardProductFeed($channel, $productsWriter);
+		// $this->groupAndStandardProductFeed($channel, $productsWriter);
 		//now do the same thing-ish for matrix items.
 		$this->matrixProductFeed($channel, $productsWriter);
 		//lastly do the same for dynamic group items.
-		$this->dynamicGroupProductFeed($channel, $productsWriter);
+		// $this->dynamicGroupProductFeed($channel, $productsWriter);
 
 		$this->endXML($productsWriter);
 
@@ -183,6 +183,7 @@ class GoogleProductsFeed
 		/** @var \Shop\Models\Products $product */
 		$productCollection = \Shop\Models\Products::collection();
 		$query = [
+			'slug' => '13brzfr-parent-sticker-fab-front-and-rear-3d-carbon-fiber-emblem-overlays',
 			'policies.group_only' => ['$ne' => 1],
 			'product_type' => 'matrix',
 			'$or' => [
@@ -273,10 +274,12 @@ class GoogleProductsFeed
 					}
 
 					//if no child has an img use the matrix parent image.
-					if (empty($imgUrl)) {
-						$imgUrl = !empty($matrix->get('google_image')) ? $matrix->get('google_image') : $matrix->get('featured_image.slug');
+					if (empty($imgUrl) && !empty($matrix->get('google_image'))) {
+						$imgUrl =  $matrix->get('google_image');
+						$imgType = 'upload';
+					}else{
+						$imgUrl =  $matrix->get('featured_image.slug');
 					}
-
 					//merge all non null values
 					$item = array_merge(array_filter([
 						'title'                     => $title,
@@ -414,8 +417,11 @@ class GoogleProductsFeed
 						}
 
 						//if no child has an img use the matrix parent image.
-						if (empty($imgUrl)) {
-							$imgUrl = !empty($dynamicGroup->get('google_image')) ? $dynamicGroup->get('google_image') : $dynamicGroup->get('featured_image.slug');
+						if (empty($imgUrl) && !empty($dynamicGroup->get('google_image'))) {
+							$imgUrl =  $dynamicGroup->get('google_image');
+							$imgType = 'upload';
+						}else{
+							$imgUrl =  $dynamicGroup->get('featured_image.slug');
 						}
 
 						//merge all non null values
