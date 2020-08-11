ecfa74bf067a1f289b65ca71a25bf35fec9b0420
Kiley Holman <kiley.holman@rallysportdirect.com>
2020-08-11 17:54:04 +0000

    Add after save check for JBA products to admin listener.

--- public/cli.php
+++ public/cli.php
@@ -118,6 +118,7 @@ $app->route('GET /listrak-datafeeds', function(){
     $app = \Base::instance();
     foreach($salesChannels as $channel){
         $app->set('sales_channel', $channel->get('slug'));
+        $app->set('listrak.suggested_product_publication_channel', $channel->get('slug'));
         (new \Shop\Services\Listtrac\DataFeeds('public/', $channel->get('domain'), $app->get('listrak.host'), $channel->get('listrak.username'), $channel->get('listrak.password')))->GenerateFeeds();
     }
 });
