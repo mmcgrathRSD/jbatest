241ba9f77ee0842c79b0474f4893af6095ec6423
Austin Roberts <austin.roberts@rallysportdirect.com>
2020-10-07 21:15:56 +0000

    I take full responsibility for what had to be added to the shop product controller to make this happen, commit 6e3da25b71b27a51b18cd0e32bd12868677bc83b

--- apps/Theme/Overrides/Shop/Site/Views/product/blocks/image_modal.php
+++ apps/Theme/Overrides/Shop/Site/Views/product/blocks/image_modal.php
@@ -1,3 +1,23 @@
+<?php //west says he's sure that the first image in the array will 110% always be the featured image. 
+$featuredImage = $item->featuredImage();
+if(!empty($images[0])) {
+    if(!empty($images[0]['alt'])) {
+        $alt = $images[0]['alt'];
+    } else {
+        $alt = $item->title;
+
+        if(!empty($item->title_suffix)) {
+            $alt .= ' - ' . $item->title_suffix;
+        }
+    }
+    
+
+    $title = $images[0]['title'] ?? $item->title . ' (Part Number: ' . $item->get('tracking.oem_model_number') . ')';
+} else {
+    $alt = $item->title;
+    $title = $item->title . ' (Part Number: ' . $item->get('tracking.oem_model_number') . ')';
+}
+?>
 <div class="product-img-box">
     <?php if($item->{'product_type'} == 'dynamic_group') : ?>
         <?php echo $this->renderLayout('Shop/Site/Views::product/kits.php')?>
@@ -6,9 +26,9 @@
     <?php echo $this->renderLayout('Shop/Site/Views::product/blocks/flag.php')?>
     <a href="#data-image-modal-main" rel="lighbox-zoom-gallery">
     
-    <img src="<?php echo $item->featuredImage(); ?>" data-srcx2="<?php echo $item->featuredImage(); ?>" alt="<?php echo $item->title; ?>" title="<?php echo $item->title; ?>" width="473" height="473">
+    <img src="<?php echo $featuredImage; ?>" data-srcx2="<?php echo $featuredImage; ?>" alt="<?php echo $alt ?>" title="<?php echo $title; ?>" width="473" height="473">
     </a>
-    <div style="display:none"><div id="data-image-modal-main"><img src="<?php echo $item->featuredImage(); ?>" alt="<?php echo $item->title; ?>" title="<?php echo $item->title; ?>"></div></div>
+    <div style="display:none"><div id="data-image-modal-main"><img src="<?php echo $featuredImage; ?>" alt="<?php echo $alt; ?>" title="<?php echo $title; ?>"></div></div>
     </div>
     <?php if (count($images) > 1) : ?>
     <div class="more-views">
@@ -22,14 +42,29 @@
         <div class="jcarousel-clip jcarousel-clip-horizontal" style="position: relative;">
             <ul class="jcarousel-slider slides jcarousel-list jcarousel-list-horizontal" id="thumbs_slider" style="overflow: hidden; position: relative; top: 0px; margin: 0px; padding: 0px; left: 0px; width: 705px;">
             <?php foreach (array_values($images) as $key=>$image) : ?>
+                <?php
+                if(!empty($image['alt'])) {
+                    $alt = $image['alt'];
+                } else {
+                    $alt = $item->title;
+
+                    if(!empty($item->title_suffix)) {
+                        $alt .= ' - ' . $item->title_suffix;
+                    }
+                }
+                
+
+                $title = $image['title'] ?? $item->title . ' (Part Number: ' . $item->get('tracking.oem_model_number') . ')';
+
+                ?>
                 <li class="jcarousel-item jcarousel-item-horizontal jcarousel-item-1 jcarousel-item-1-horizontal" jcarouselindex="1" style="float: left; list-style: none;">
                 <a href="#data-image-modal-<?php echo $key; ?>" class="lighbox-zoom-gallery" rel="lighbox-zoom-gallery" title="">
                 <span style="width: 92px; height: 92px;"></span>
-                <img alt="<?php echo $item->title; if(!empty($item->title_suffix)) { echo ' - ' . $item->title_suffix; } ?>" title="<?php echo $item->title; ?> (Part Number: <?php echo $item->get('tracking.oem_model_number'); ?>)" src="<?php echo $item->product_image($image, [
+                <img alt="<?php echo $alt; ?>" title="<?php echo $title; ?>" src="<?php echo $item->product_image($image['image'], [
                     'transformation' => \Base::instance()->get('cloudinary.product_page_thumb')
-                ]);?>" data-srcx2="<?php echo $item->product_image($image)?>" width="110" height="110" alt="">
+                ]);?>" data-srcx2="<?php echo $item->product_image($image['image'])?>" width="110" height="110">
                 </a>
-                <div style="display:none"><div id="data-image-modal-<?php echo $key; ?>"><img src="<?php echo $item->product_image($image, [
+                <div style="display:none"><div id="data-image-modal-<?php echo $key; ?>"><img alt="<?php echo $alt; ?>" title="<?php echo $title; ?>" src="<?php echo $item->product_image($image['image'], [
                     'transformation' => \Base::instance()->get('cloudinary.product')
                 ]);?>" /></div></div>
                 </li>
